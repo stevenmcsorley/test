7ee6acfdac0c6cd28055df8b75edc54b83613d30
Steven McSorley <steven.mcsorley@eqtr.com>
2022-06-16 02:16:12 +0100

    refactor: :zap: Sass tidy up

--- src/js/filter/filter.js
+++ src/js/filter/filter.js
@@ -1,14 +1,18 @@
 export const filter = () => {
   const industrySelect = document.querySelector("[js-data-select]");
   const cards = document.querySelectorAll("[data-industry]");
+  var delayInMilliseconds = 300;
 
   const filterCards = () => {
     let value = industrySelect.value;
     cards.forEach(function (card) {
       if (card.dataset.industry === value || value === "all") {
-        card.classList.remove("hidden");
+        card.classList.remove("hidden", "visualhidden");
       } else {
-        card.classList.add("hidden");
+        card.classList.add("visualhidden");
+        setTimeout(function () {
+          card.classList.add("hidden");
+        }, delayInMilliseconds);
       }
     });
   };
