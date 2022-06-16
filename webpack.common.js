1595bfa8e75d42e4de6ce813e472577890587bea
Steven McSorley <steven.mcsorley@eqtr.com>
2022-06-16 02:35:44 +0100

    build: :zap: Compress images in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-
-
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-index: "./src/js/index.js",
+    index: "./src/js/index.js",
   },
   module: {
     rules: [
@@ -22,37 +21,71 @@ index: "./src/js/index.js",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        type: "asset/resource",
+      },
     ],
   },
   optimization: {
-    splitChunks: {
-      chunks: "all",
-        cacheGroups: {
-            vendor: {
-                test: /[\\/]node_modules[\\/]/,
-                priority: -10,
-                name: "vendor",
-                enforce: true,
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                // That setting might be close to lossless, but it’s not guaranteed
+                // https://github.com/GoogleChromeLabs/squoosh/issues/85
+                quality: 100,
+              },
+              webp: {
+                lossless: 1,
+              },
+              avif: {
+                // https://github.com/GoogleChromeLabs/squoosh/blob/dev/codecs/avif/enc/README.md
+                cqLevel: 0,
+              },
             },
+          },
         },
-
+      }),
+    ],
+    splitChunks: {
+      chunks: "async",
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
+  },
+  performance: {
+    hints: false,
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(
-    {
-        template: './src/index.html',
-        inject: true,
-        chunks: ['index'],
-        filename: 'index.html'
-    }
-  )],
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: 'src/assets/images/[name].[ext]'
+    assetModuleFilename: "src/assets/images/[name].[ext]",
   },
 };
